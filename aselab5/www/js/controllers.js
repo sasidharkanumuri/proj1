cf4ffdcccc71f9bbab42af8f4dc65d53727f5950
sasidharkanumuri <kanumuri1994@gmail.com>
2015-10-02 14:36:05 -0500

    lab5

---
+++ aselab5/www/js/controllers.js
@@ -0,0 +1,286 @@
+angular.module('stockWatch.controllers', [])
+
+.controller('AppCtrl', ['$scope','modalService',
+  function($scope, modalService) {
+
+    $scope.modalService = modalService;
+
+}])
+
+.controller('MyStocksCtrl', ['$scope', 'myStocksArrayService',
+  function($scope, myStocksArrayService) {
+
+    $scope.myStocksArray = myStocksArrayService;
+    console.log(myStocksArrayService);
+
+}])
+
+
+ .controller('myController',function($scope){
+	   
+	   $scope.cnames=[{company:'IBM'}];
+		   $scope.companyname=function(company){
+			   return company;
+			   
+		   }
+   })
+   
+
+.controller('StockCtrl', ['$scope','$window', '$ionicPopup', '$stateParams', 'stockDataService', 'dateService', 'chartDataService', 'notesService', 'newsService', 'followStocksService',
+   function($scope, $window, $ionicPopup, $stateParams, stockDataService, dateService, chartDataService, notesService, newsService, followStocksService) {
+
+     $scope.ticker = $stateParams.stockTicker;
+     $scope.chartView = 4;
+     $scope.oneYearAgoDate = dateService.oneYearAgoDate();
+     $scope.todayDate = dateService.currentDate();
+
+     $scope.stockNotes=[];
+
+     $scope.following = followStocksService.checkFollowing($scope.ticker);
+
+
+
+     $scope.$on('$ionicView.afterEnter', function(){
+       getPriceData();
+       getDetailsData();
+       getChartData();
+       getNews();
+       $scope.stockNotes = notesService.getNotes($scope.ticker);
+     });
+
+     $scope.toggleFollow = function(){
+       if ($scope.following) {
+         followStocksService.unfollow($scope.ticker);
+         $scope.following=false;
+       } else{
+         followStocksService.follow($scope.ticker);
+         $scope.following = true;
+       }
+     };
+
+     $scope.chartViewFunc=function(inp){
+       $scope.chartView=inp;
+     };
+
+     $scope.openWindow= function(link){
+       //TODO install and setup in app browser
+       console.log("openWindow ->" + link);
+       console.log($scope.newsStories[3]);
+     };
+
+
+ $scope.addNote = function() {
+  $scope.note = {title: 'Note', body: '', date: $scope.todayDate, ticker: $scope.ticker};
+
+  // An elaborate, custom popup
+  var note = $ionicPopup.show({
+    template: '<input type="text" ng-model="note.title" id="stock-note-title"><textarea type="text" ng-model="note.body" id="stock-note-body"></textarea>',
+    title: 'New Note for '+ $scope.ticker,
+    scope: $scope,
+    buttons: [
+      { text: 'Cancel',
+        onTap: function(e){
+          return;
+        }
+     },
+      {
+        text: '<b>Save</b>',
+        type: 'button-balanced',
+        onTap: function(e) {
+        notesService.addNote($scope.ticker, $scope.note);
+        }
+      }
+    ]
+  });
+  note.then(function(res) {
+    $scope.stockNotes= notesService.getNotes($scope.ticker);
+  });
+};
+
+
+$scope.openNote = function(index, body, title) {
+ $scope.note = {title: title, body: body, date: $scope.todayDate, ticker: $scope.ticker};
+
+ // An elaborate, custom popup
+ var note = $ionicPopup.show({
+   template: '<input type="text" ng-model="note.title" id="stock-note-title"><textarea type="text" ng-model="note.body" id="stock-note-body"></textarea>',
+   title: $scope.note.title,
+   scope: $scope,
+   buttons: [
+     {
+       text: 'Delete',
+       type: 'button-assertive button-small',
+       onTap: function(e){
+         notesService.deleteNote($scope.ticker, index);
+       }
+     },
+     { text: 'Cancel',
+       type: 'button-small',
+       onTap: function(e){
+         return;
+       }
+    },
+     {
+       text: '<b>Save</b>',
+       type: 'button-balanced button-small',
+       onTap: function(e) {
+       notesService.deleteNote($scope.ticker, index);
+       notesService.addNote($scope.ticker, $scope.note);
+       }
+     }
+   ]
+ });
+ note.then(function(res) {
+   $scope.stockNotes= notesService.getNotes($scope.ticker);
+ });
+};
+
+
+  function getNews(){
+    $scope.newsStories=[];
+
+    var promise = newsService.getNews($scope.ticker);
+
+    promise.then(function(data){
+      $scope.newsStories = data;
+    });
+  }
+
+
+
+     function getPriceData(){
+
+
+     var promise = stockDataService.getPriceData($scope.ticker);
+     promise.then(function(data){
+       $scope.stockPriceData = data;
+
+       if(data.chg_percent>=0 && data !== null){
+         $scope.reactiveColor = {'background-color':'#33cd5f', 'border-color' : 'rgba(255,255,255,.3)'};
+       } else if(data.chg_percent <=0 && data !== null){
+         $scope.reactiveColor = {'background-color':'#ef473a', 'border-color' : 'rgba(0,0,0,.2)'};
+       }
+     });
+   }
+
+   function getDetailsData(){
+
+   var promise = stockDataService.getDetailsData($scope.ticker);
+   promise.then(function(data){
+
+     //console.log(data);
+
+     $scope.stockDetailsData = data;
+   });
+   }
+
+   function getChartData(){
+     var promise = chartDataService.getHistoricalData($scope.ticker, $scope.oneYearAgoDate, $scope.todayDate);
+
+     promise.then(function(data){
+
+       $scope.myData = JSON.parse(data)
+     .map(function(series) {
+       series.values = series.values.map(function(d) { return {x: d[0], y: d[1] }; });
+       return series;
+     });
+
+     });
+   }
+
+
+   //NV-CHART EXAMPLE
+
+
+
+	var xTickFormat = function(d) {
+		var dx = $scope.myData[0].values[d] && $scope.myData[0].values[d].x || 0;
+		if (dx > 0) {
+      return d3.time.format("%b %d")(new Date(dx));
+		}
+		return null;
+	};
+
+  var x2TickFormat = function(d) {
+    var dx = $scope.myData[0].values[d] && $scope.myData[0].values[d].x || 0;
+    return d3.time.format('%b %Y')(new Date(dx));
+  };
+
+  var y1TickFormat = function(d) {
+    return d3.format(',f')(d);
+  };
+
+  var y2TickFormat = function(d) {
+    return d3.format('s')(d);
+  };
+
+  var y3TickFormat = function(d) {
+    return d3.format(',.2s')(d);
+  };
+
+  var y4TickFormat = function(d) {
+    return d3.format(',.2s')(d);
+  };
+
+  var xValueFunction = function(d, i) {
+    return i;
+  };
+
+  var marginBottom=($window.innerWidth / 100) * 10;
+
+	$scope.chartOptions = {
+    chartType: 'linePlusBarWithFocusChart',
+    data: 'myData',
+    margin: {top: 15, right: 0, bottom: marginBottom, left: 0},
+    interpolate: "cardinal",
+    useInteractiveGuideline: false,
+    yShowMaxMin: false,
+    tooltips: false,
+    showLegend: false,
+    useVoronoi: false,
+    xShowMaxMin: false,
+    xValue: xValueFunction,
+    xAxisTickFormat: xTickFormat,
+    x2AxisTickFormat: x2TickFormat,
+    y1AxisTickFormat: y1TickFormat,
+    y2AxisTickFormat: y2TickFormat,
+    y3AxisTickFormat: y3TickFormat,
+    y4AxisTickFormat: y4TickFormat,
+    transitionDuration: 500,
+    y1AxisLabel: 'Price',
+    y3AxisLabel: 'Volume',
+    noData: 'Loading Data...'
+	};
+
+}])
+
+.controller('SearchCtrl', ['$scope','$state', 'modalService','searchService',
+function($scope, $state, modalService, searchService){
+
+  $scope.closeModal = function(){
+    modalService.closeModal();
+  };
+
+  $scope.search = function(){
+    $scope.searchResults='';
+    startSearch($scope.searchQuery);
+  };
+
+  var startSearch = ionic.debounce(function(query){
+    searchService.search(query)
+    .then(function(data){
+      $scope.searchResults=data;
+    });
+  }, 750);
+
+  $scope.goToStock = function(ticker){
+
+    modalService.closeModal();
+    $state.go('app.stock', {stockTicker: ticker});
+
+
+  };
+
+}])
+
+;
---
+++ aselab5/www/js/services.js
@@ -0,0 +1,446 @@
+angular.module('stockWatch.services', [])
+
+.factory('encodeURIService', function(){
+
+  return {
+    encode: function(string){
+      console.log(string);
+      return encodeURIComponent(string).replace(/\"/g, "%22").replace(/\ /g, "%20").replace(/[!'()]/g, escape);
+    }
+  };
+
+})
+
+
+.factory('dateService', function($filter){
+
+  var currentDate = function(){
+    var d = new Date();
+    var date = $filter('date')(d,'yyyy-MM-dd');
+    return date;
+  };
+
+  var oneYearAgoDate = function(){
+    var d = new Date(new Date().setDate(new Date().getDate() - 365));
+    var date = $filter('date')(d, 'yyyy-MM-dd');
+    return date;
+
+  };
+
+  return {
+    currentDate: currentDate,
+    oneYearAgoDate: oneYearAgoDate
+  };
+
+})
+
+.factory('stockDetailsCacheService', function(CacheFactory){
+
+  var stockDeatailsCache;
+
+  if(!CacheFactory.get('stockDeatailsCache')){
+    stockDeatailsCache = CacheFactory('stockDeatailsCache', {
+      maxAge: 60 * 1000,
+      deleteOnExpire:'aggressive',
+      storageMode: 'localStorage'
+    });
+  }else{
+    stockDeatailsCache = CacheFactory.get('stockDeatailsCache');
+  }
+
+  return stockDeatailsCache;
+
+})
+
+.factory('stockDataService', function($q,$http, encodeURIService, stockDetailsCacheService){
+
+  var getDetailsData=function(ticker){
+
+    var deferred=$q.defer(),
+
+    cacheKey = ticker,
+    stockDeatailsCache = stockDetailsCacheService.get(cacheKey),
+    query = 'select * from yahoo.finance.quotes where symbol IN ("' + ticker + '")',
+    url = 'http://query.yahooapis.com/v1/public/yql?q='+ encodeURIService.encode(query) + '&format=json&env=http://datatables.org/alltables.env';
+
+    //console.log(url);
+if(stockDeatailsCache){
+  deferred.resolve(stockDeatailsCache);
+}else {
+    $http.get(url)
+      .success(function(json){
+        var jsonData = json.query.results.quote;
+        deferred.resolve(jsonData);
+
+        stockDetailsCacheService.put(cacheKey, jsonData);
+      })
+      .error(function(err){
+          console.log("Details Data Error"+ err);
+          deferred.reject();
+    });
+}
+
+      return deferred.promise;
+  };
+
+
+ var getPriceData=function(ticker){
+
+   var deferred = $q.defer(),
+       url='http://finance.yahoo.com/webservice/v1/symbols/'+ ticker + '/quote?format=json&view=detail';
+
+      $http.get(url)
+        .success(function(json){
+          var jsonData = json.list.resources[0].resource.fields;
+          deferred.resolve(jsonData);
+      })
+        .error(function(err){
+            console.log("Price Data Error"+ err);
+            deferred.reject();
+  });
+
+  return deferred.promise;
+
+};
+
+  return {
+    getPriceData: getPriceData,
+    getDetailsData: getDetailsData
+  };
+})
+
+
+.factory('chartDataCacheService', function(CacheFactory){
+
+  var chartDataCache;
+
+  if(!CacheFactory.get('chartDataCache')){
+    chartDataCache = CacheFactory('chartDataCache', {
+      maxAge: 60 * 60 * 8 * 1000,
+      deleteOnExpire:'aggressive',
+      storageMode: 'localStorage'
+    });
+  } else {
+    chartDataCache = CacheFactory.get('chartDataCache');
+  }
+
+  return chartDataCache;
+
+})
+
+.factory('chartDataService', function($q, $http, encodeURIService, chartDataCacheService){
+
+  var getHistoricalData = function(ticker, fromDate, todayDate){
+
+    var deferred = $q.defer(),
+
+    cacheKey = ticker,
+    chartDataCache = chartDataCacheService.get(cacheKey),
+    query= 'select * from yahoo.finance.historicaldata where symbol = "'+ ticker + '" and startDate = "'+ fromDate + '" and endDate = "'+ todayDate +'"',
+    url = 'http://query.yahooapis.com/v1/public/yql?q='+ encodeURIService.encode(query) + '&format=json&env=http://datatables.org/alltables.env';
+
+if(chartDataCache){
+  deferred.resolve(chartDataCache);
+}else{
+    $http.get(url)
+
+      .success(function(json){
+        console.log(json);
+        var jsonData=json.query.results.quote;
+
+        var priceData=[],
+            volumeData=[];
+
+        jsonData.forEach(function(dayDataObject){
+
+          var dateToMillis = dayDataObject.Date,
+          date = Date.parse(dateToMillis),
+          price = parseFloat(Math.round(dayDataObject.Close * 100) / 100).toFixed(3),
+          volume = dayDataObject.Volume,
+
+          volumeDatum = '['+ date + ',' + volume + ']',
+          priceDatum = '['+ date + ',' + price + ']';
+
+          //console.log(volumeDatum, priceDatum);
+
+          volumeData.unshift(volumeDatum);
+          priceData.unshift(priceDatum);
+
+        });
+
+        var formattedChartData =
+        '[{' +
+          '"key":' + '"volume",' +
+          '"bar":' + 'true,' +
+          '"values":' + '[' + volumeData + ']' +
+          '},' +
+          '{' +
+          '"key":' + '"' + ticker + '",' +
+          '"values":' + '[' + priceData + ']' +
+        '}]';
+
+      deferred.resolve(formattedChartData);
+      chartDataCacheService.put(cacheKey, formattedChartData);
+
+      })
+      .error(function(err){
+        console.log('chartsDataService Error'+ err);
+        deferred.reject();
+      });
+    }
+
+      return deferred.promise;
+
+  };
+
+  return{
+    getHistoricalData: getHistoricalData
+  };
+
+
+
+})
+
+.factory('notesCacheService', function(CacheFactory){
+
+  var notesCache;
+
+  if(!CacheFactory.get('notesCache')){
+    notesCache = CacheFactory('notesCache', {
+      storageMode: 'localStorage'
+    });
+  }else{
+    notesCache = CacheFactory.get('notesCache');
+  }
+  return notesCache;
+})
+
+.factory('notesService', function(notesCacheService){
+
+  return {
+
+    getNotes: function(ticker){
+      return notesCacheService.get(ticker);
+    },
+
+    addNote: function(ticker, note){
+
+      var stockNotes=[];
+
+      if(notesCacheService.get(ticker)){
+        stockNotes=notesCacheService.get(ticker);
+        stockNotes.push(note);
+      }else{
+        stockNotes.push(note);
+      }
+      notesCacheService.put(ticker, stockNotes);
+    },
+
+    deleteNote: function(ticker, index){
+
+      var stockNotes = [];
+
+      stockNotes = notesCacheService.get(ticker);
+      stockNotes.splice(index, 1);
+      notesCacheService.put(ticker, stockNotes);
+
+    }
+
+  };
+
+})
+
+.factory('newsService', function($q, $http){
+
+  return {
+
+    getNews: function(ticker){
+      var deferred = $q.defer(),
+      x2js = new X2JS(),
+
+      url='http://finance.yahoo.com/rss/headline?s='+ ticker;
+
+      $http.get(url)
+      .success(function(xml){
+
+        var xmlDoc=x2js.parseXmlString(xml),
+        json = x2js.xml2json(xmlDoc),
+        jsonData = json.rss.channel.item;
+
+        deferred.resolve(jsonData);
+
+      })
+      .error(function(err){
+        deferred.reject();
+        console.log('News Service Error', err);
+      });
+
+      return deferred.promise;
+    }
+
+  };
+
+})
+
+.factory('fillMyStocksCacheService', function(CacheFactory){
+
+  var myStocksCache;
+
+  if(!CacheFactory.get('myStocksCache')){
+    myStocksCache = CacheFactory('myStocksCache', {
+      storageMode: 'localStorage'
+    });
+  }else{
+    myStocksCache=CacheFactory.get(myStocksCache);
+  }
+
+  var fillMyStocksCache = function(){
+    var myStocksArray=[
+      {ticker:'AAPL'},
+      {ticker:'GPRO'},
+      {ticker:'FB'},
+      {ticker:'NFLX'},
+      {ticker:'TSLA'},
+      {ticker:'BRK-A'},
+      {ticker:'INTC'},
+      {ticker:'MSFT'},
+      {ticker:'GE'},
+      {ticker:'BAC'},
+      {ticker:'C'},
+      {ticker:'T'}
+    ];
+
+    myStocksCache.put('myStocks', myStocksArray);
+  };
+
+  return {
+    fillMyStocksCache: fillMyStocksCache
+  };
+
+})
+
+.factory('myStocksCacheService', function(CacheFactory){
+
+  var myStocksCache = CacheFactory.get('myStocksCache');
+
+  return myStocksCache;
+
+})
+
+.factory('myStocksArrayService', function(fillMyStocksCacheService, myStocksCacheService){
+
+  if(!myStocksCacheService.info('myStocks')){
+    fillMyStocksCacheService.fillMyStocksCache();
+  }
+
+  var myStocks=myStocksCacheService.get('myStocks');
+
+  return myStocks;
+
+})
+
+.factory('followStocksService', function(myStocksArrayService, myStocksCacheService){
+
+  return {
+
+    follow:function(ticker){
+
+      var stockToAdd = {'ticker': ticker};
+
+
+      myStocksArrayService.push(stockToAdd);
+      myStocksCacheService.put('myStocks', myStocksArrayService);
+
+    },
+
+    unfollow:function(ticker){
+      for (var i = 0; i < myStocksArrayService.length; i++) {
+        if(myStocksArrayService[i].ticker == ticker){
+          myStocksArrayService.splice(i, 1);
+          myStocksCacheService.remove('myStocks');
+          myStocksCacheService.put('myStocks', myStocksArrayService);
+
+          break;
+        }
+      }
+    },
+
+    checkFollowing:function(ticker){
+      for (var i = 0; i < myStocksArrayService.length; i++) {
+        if (myStocksArrayService[i].ticker == ticker) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+  };
+
+})
+
+.service('modalService', function($ionicModal){
+
+  this.openModal = function(id){
+    var _this = this;
+
+    if(id==1){
+    $ionicModal.fromTemplateUrl('templates/search.html', {
+      scope: null,
+      controller: 'SearchCtrl'
+    }).then(function(modal) {
+      _this.modal = modal;
+      _this.modal.show();
+    });
+  }else if(id==2){
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+  }else if(id ==3){
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+  }
+  };
+
+  this.closeModal = function(){
+    var _this = this;
+
+    if(!_this.modal) return;
+    _this.modal.hide();
+    _this.modal.remove();
+  };
+
+})
+
+.factory('searchService', function($q, $http){
+
+  return {
+    search: function(query){
+      var deferred = $q.defer(),
+
+      url='http://d.yimg.com/autoc.finance.yahoo.com/autoc?query="'+ query +'"&callback=YAHOO.Finance.SymbolSuggest.ssCallback';
+
+      YAHOO = window.YAHOO = {
+        Finance : {
+          SymbolSuggest: {}
+        }
+      };
+
+      YAHOO.Finance.SymbolSuggest.ssCallback = function(data){
+        var jsonData = data.ResultSet.Result;
+        deferred.resolve(jsonData);
+      };
+      $http.jsonp(url)
+      .then(YAHOO.Finance.SymbolSuggest.ssCallback);
+
+      return deferred.promise;
+    }
+  };
+
+})
+
+;
